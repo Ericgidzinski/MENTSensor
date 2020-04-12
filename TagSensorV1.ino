9fecd5da78eb78fdb18817b76d3f50be6e0f86e0
Ericgidzinski <39743561+Ericgidzinski@users.noreply.github.com>
2020-04-12 08:37:07 -0400

    Adjusted pinout and added ability to play message

--- TagSensorV1.ino
+++ TagSensorV1.ino
@@ -6,13 +6,14 @@ Team #11 - Trinia Medrano, Akshansh Gupta, Eric Gidzinski
 
 PINOUT:
   ESP8266
-    TX: 8
-    RX: 9
+    TX: A1
+    RX: A0
     Power: 3.3V
+    CH_EN: 3.3V
     GND: GND
 
   Speaker
-    Red: 10
+    Red: 9
     GND: GND
 
   SD Card
@@ -24,7 +25,7 @@ PINOUT:
     GND:  GND
 
   PIR Sensor
-    Out: 7
+    Out: 6
     VCC: 5V
     GND: GND
     
@@ -37,40 +38,21 @@ PINOUT:
 #include <SPI.h>
 
 #define SD_ChipSelectPin 4   // Defines CS pin
-#define PIR 7                // Define motion sensor pin
-#define SPEAKER 10  // To Do: Remove once testing is done
+#define PIR 6                // Define motion sensor pin
+#define SPEAKER 9  // To Do: Remove once testing is done
 #define BEATTIME 200 // To Do: Remove once testing is done
 TMRpcm speaker;              // Create object for speaker library
-ESP8266_Simple ESP8266(8,9); // TX, RX
+ESP8266_Simple ESP8266(A1,A0); // TX, RX
 
 char espBuffer[300];
 byte responseCode;
 int motion = 0;     // Variable for reading status of motion sensor
 int pirState = LOW; // Start by assuming no motion detected
 
-//void ESP8266Command(char* command)
-//{
-//  if((responseCode = ESP8266.sendCommand(F(command), buffer, sizeof(buffer))) == ESP8266_OK)
-//  {
-//    // The command worked, prnt the result
-//    Serial.println("OK");
-//    Serial.println(buffer);
-//  }
-//  else
-//  {
-//    // An error occurred, would you like to see what
-//    ESP8266.debugPrintError(responseCode, &Serial);
-//    
-//    // Or you can get it as a string if you prefer
-//    char bufferWithMinLength50Char[50];
-//    ESP8266.getErrorMessage(responseCode, bufferWithMinLength50Char);    
-//  }
-//}
-
 void initiateESP8266()
 {
   // Start communication with ESP8266
-  ESP8266.begin(115200);
+  ESP8266.begin(9600);
   Serial.println("Test communication w/ ESP8266\n");
   delay(1000);
   if((responseCode = ESP8266.sendCommand(F("AT"), espBuffer, sizeof(espBuffer))) == ESP8266_OK)
@@ -107,6 +89,24 @@ void initiateESP8266()
     ESP8266.getErrorMessage(responseCode, bufferWithMinLength50Char);    
   }
   delay(1000);
+
+  Serial.println("Setting Baud to 9600 (easier for software serial)\n");
+  if((responseCode = ESP8266.sendCommand(F("AT+UART_DEF=9600,8,1,0,0"), espBuffer, sizeof(espBuffer))) == ESP8266_OK)
+  {
+    // The command worked, prnt the result
+    Serial.println("OK");
+    Serial.println(espBuffer);
+  }
+  else
+  {
+    // An error occurred, would you like to see what
+    ESP8266.debugPrintError(responseCode, &Serial);
+    
+    // Or you can get it as a string if you prefer
+    char bufferWithMinLength50Char[50];
+    ESP8266.getErrorMessage(responseCode, bufferWithMinLength50Char);    
+  }
+  delay(1000);
   
   Serial.println("Changing to Station and Access Point \n");
   if((responseCode = ESP8266.sendCommand(F("AT+CWMODE=?"), espBuffer, sizeof(espBuffer))) == ESP8266_OK)
@@ -127,7 +127,7 @@ void initiateESP8266()
   delay(1000);
   
   Serial.println("Creating MENT Sensor Access Point\n");
-  if((responseCode = ESP8266.sendCommand("AT+CWSAP=\"MENT Sensor\",\"\",1,0,4,0", espBuffer, sizeof(espBuffer)),1) == ESP8266_OK)
+  if((responseCode = ESP8266.sendCommand("AT+CWSAP=\"MENTXYZ Sensor\",\"\",1,0,4,0", espBuffer, sizeof(espBuffer)),1) == ESP8266_OK)
   {
     // The command worked, prnt the result
     Serial.println("OK");
@@ -150,6 +150,7 @@ void initiateESP8266()
 void playMessage() 
 {
   // To Do: Figure out naming system of saved messages
+  speaker.play("1.wav");
   return;
 }
 
@@ -184,12 +185,15 @@ void detectMotion()
 }
 
 // TO DO: Function that pulls audio file from app 
+// Pulls audio via GET requests
+// TO DO: Add name of sensor as an input to the function. Used to pull correct file from app.
 void pullAudio()
 {
   
 }
 
 // TO DO: Function that runs when first pairing the sensor to the app
+// TO DO: Add a way (maybe a button) to reset the sensor and to enter reset mode
 void pairSensor()
 {
   
@@ -197,21 +201,31 @@ void pairSensor()
 
 void setup()
 {
-  Serial.begin(115200); // TO DO: Delete once remote WiFi setup works
+  Serial.begin(9600); // TO DO: Delete once remote WiFi setup works
 
+  // Start the SD card
+  if (!SD.begin(SD_ChipSelectPin)) {      //see if the card is present and can be initialized
+    Serial.println("SD fail");
+    return;                               //don't do anything more if not
+  }
+  
   // Initiate speaker
-  speaker.setVolume(5); // 0 - 5. Set volume level
+  speaker.speakerPin = SPEAKER;
+  speaker.setVolume(6); // 0 - 6. Set volume level
 
   // Initiate ESP8266
-  initiateESP8266();
-  
+//  initiateESP8266(); TO DO: Figure out why ESP8266 cannot run the same time as TMRpcm speaker. Maybe SPI issue?
+
   // Initiate PIR sensor
   pinMode(PIR, INPUT);
   
   Serial.println("Initiation complete");
+
+  // Pull latest 
+  pullAudio();
 }
 
 void loop()
 {
-//  detectMotion();
+  detectMotion();
 }
